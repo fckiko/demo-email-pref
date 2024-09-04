62cafb714d7be5b9334f989a893c25805cfd990e
Francis <francis@serverless-secure.com-at-025181935248>
2024-09-04 12:21:11 +0800

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { BidsNBuysBoardComponent } from "./export-result/bids-n-buys-board/bids-n-buys-board.component";
+import { DesktopNavForUserComponent } from "./export-result/desktop-nav-for-user/desktop-nav-for-user.component";
+import { FooterDesktopComponent } from "./export-result/footer-desktop/footer-desktop.component";
+import { Frame571Component } from "./export-result/frame-571/frame-571.component";
+import { ToggleAllComponent } from "./export-result/toggle-all/toggle-all.component";
+import { NavListComponent } from "./export-result/nav-list/nav-list.component";
+import { ToggleButtonComponent } from "./export-result/toggle-button/toggle-button.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BidsNBuysBoardComponent,
+    DesktopNavForUserComponent,
+    FooterDesktopComponent,
+    Frame571Component,
+    ToggleAllComponent,
+    NavListComponent,
+    ToggleButtonComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
