ab018984ceaf238ac73ddfecd51f218707ba0028
Francis <francis@serverless-secure.com-at-025181935248>
2024-09-05 17:35:00 +0800

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo-email-pref';
+  emailPreferencesForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private http:HttpClient) {
+    this.emailPreferencesForm = this.fb.group({
+      emailMarketing: [true],
+      personalInsight: [true],
+      eventInvitations: [false],
+      researchFeedback: [true],
+      unsubscribeAll: [false],
+
+    });
+  }
+
+  togglePreference(category: string) {
+    console.log(`${category} preference changed:`, this.emailPreferencesForm.controls[category].value);
+    this.sendToApi()
+  }
+
+  updatePreferences() {
+    console.log("Preferences updated", this.emailPreferencesForm.value);
+    this.sendToApi()
+  }
+  sendToApi(){
+    this.http.post('https://jsonplaceholder.typicode.com/posts', {
+      data:this.emailPreferencesForm.value
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,23 @@
-import { NgModule } from "@angular/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { AppComponent } from "./app.component";
-import { HttpClientModule } from "@angular/common/http";
-import { RouterModule } from "@angular/router";
-import { BidsNBuysBoardComponent } from "./pages/bids-n-buys-board/bids-n-buys-board.component";
-import { DesktopNavForUserComponent } from "./pages/desktop-nav-for-user/desktop-nav-for-user.component";
-import { FooterDesktopComponent } from "./pages/footer-desktop/footer-desktop.component";
-import { Frame571Component } from "./pages/frame-571/frame-571.component";
-import { ToggleAllComponent } from "./pages/toggle-all/toggle-all.component";
-import { NavListComponent } from "./pages/nav-list/nav-list.component";
-import { ToggleButtonComponent } from "./pages/toggle-button/toggle-button.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BidsNBuysBoardComponent,
-    DesktopNavForUserComponent,
-    FooterDesktopComponent,
-    Frame571Component,
-    ToggleAllComponent,
-    NavListComponent,
-    ToggleButtonComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([]),
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    InputSwitchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
